3a4e946307ed781c413639056b8e27eec8f2adc1
Bruno Bertolini <dev@brunobertolini.com>
2018-08-10 08:59:52 -0300

    feat: spacing rules

--- src/rules/spacing/index.js
+++ src/rules/spacing/index.js
@@ -1,62 +1,101 @@
 import PropTypes from "prop-types";
 
-const propTypes = {
-	margin: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+const types = [PropTypes.bool, PropTypes.string, PropTypes.number];
 
-	padding: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+const propTypes = prop => ({
+	[prop]: PropTypes.oneOfType(types),
+	[`${prop}Top`]: PropTypes.oneOfType(types),
+	[`${prop}Bottom`]: PropTypes.oneOfType(types),
+	[`${prop}Left`]: PropTypes.oneOfType(types),
+	[`${prop}Right`]: PropTypes.oneOfType(types),
+	[`${prop}Horizontal`]: PropTypes.oneOfType(types),
+	[`${prop}Vertical`]: PropTypes.oneOfType(types)
+});
 
-	marginSize: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+const defaultProps = prop => ({
+	[prop]: false,
+	[`${prop}Top`]: false,
+	[`${prop}Bottom`]: false,
+	[`${prop}Left`]: false,
+	[`${prop}Right`]: false,
+	[`${prop}Horizontal`]: false,
+	[`${prop}Vertical`]: false
+});
 
-	paddingSize: PropTypes.oneOfType([PropTypes.bool, PropTypes.string])
-};
-
-const defaultProps = {
-	margin: false,
-	padding: false,
-	marginSize: false,
-	paddingSize: false
+const choose = {
+	boolean: (props, prop) => props.theme.dimensions[prop],
+	string: (props, prop, value) => value,
+	number: (props, prop, value) => `${props.theme.dimensions.em(value)}px`
 };
 
 const rules = prop => ({
 	[prop]: {
 		_: props => `${prop}: ${props[prop]};`,
-		true: props =>
-			`${prop}: ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
-		vertical: props =>
-			`${prop}: ${props[`${prop}Size`] || props.theme.spacing[prop]} 0;`,
-		horizontal: props =>
-			`${prop}: 0 ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
-		top: props =>
-			`${prop}-top: ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
-		bottom: props =>
-			`${prop}-bottom: ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
-		left: props =>
-			`${prop}-left: ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
-		right: props =>
-			`${prop}-right: ${props[`${prop}Size`] || props.theme.spacing[prop]};`,
+		true: props => `${prop}: ${props.theme.dimensions[prop]};`,
+		vertical: props => `${prop}: ${props.theme.dimensions[prop]} 0;`,
+		horizontal: props => `${prop}: 0 ${props.theme.dimensions[prop]};`,
+		top: props => `${prop}-top: ${props.theme.dimensions[prop]};`,
+		bottom: props => `${prop}-bottom: ${props.theme.dimensions[prop]};`,
+		left: props => `${prop}-left: ${props.theme.dimensions[prop]};`,
+		right: props => `${prop}-right: ${props.theme.dimensions[prop]};`,
 		noTop: props =>
-			`${prop}: 0 ${props[`${prop}Size`] || props.theme.spacing[prop]} ${props[
-				`${prop}Size`
-			] || props.theme.spacing[prop]};`,
+			`${prop}: 0 ${props.theme.dimensions[prop]} ${
+				props.theme.dimensions[prop]
+			};`,
 		noBottom: props =>
-			`${prop}: ${props[`${prop}Size`] || props.theme.spacing[prop]} ${props[
-				`${prop}Size`
-			] || props.theme.spacing[prop]} 0;`,
+			`${prop}: ${props.theme.dimensions[prop]} ${
+				props.theme.dimensions[prop]
+			} 0;`,
 		noLeft: props =>
-			`${prop}: ${props[`${prop}Size`] || props.theme.spacing[prop]} ${props[
-				`${prop}Size`
-			] || props.theme.spacing[prop]} ${props[`${prop}Size`] ||
-				props.theme.spacing[prop]} 0;`,
+			`${prop}: ${props.theme.dimensions[prop]} ${
+				props.theme.dimensions[prop]
+			} ${props.theme.dimensions[prop]} 0;`,
 		noRight: props =>
-			`${prop}: ${props[`${prop}Size`] || props.theme.spacing[prop]} 0 ${props[
-				`${prop}Size`
-			] || props.theme.spacing[prop]} ${props[`${prop}Size`] ||
-				props.theme.spacing[prop]};`
-	}
+			`${prop}: ${props.theme.dimensions[prop]} 0 ${
+				props.theme.dimensions[prop]
+			} ${props.theme.dimensions[prop]};`
+	},
+
+	[`${prop}Top`]: props =>
+		`${prop}-top: ${choose[typeof props[`${prop}Top`]](
+			props,
+			prop,
+			props[`${prop}Top`]
+		)};`,
+	[`${prop}Right`]: props =>
+		`${prop}-right: ${choose[typeof props[`${prop}Right`]](
+			props,
+			prop,
+			props[`${prop}Right`]
+		)};`,
+	[`${prop}Bottom`]: props =>
+		`${prop}-bottom: ${choose[typeof props[`${prop}Bottom`]](
+			props,
+			prop,
+			props[`${prop}Bottom`]
+		)};`,
+	[`${prop}Left`]: props =>
+		`${prop}-left: ${choose[typeof props[`${prop}Left`]](
+			props,
+			prop,
+			props[`${prop}Left`]
+		)};`,
+	[`${prop}Vertical`]: props =>
+		`${prop}: ${choose[typeof props[`${prop}Vertical`]](
+			props,
+			prop,
+			props[`${prop}Vertical`]
+		)} 0;`,
+	[`${prop}Horizontal`]: props =>
+		`${prop}: 0 ${choose[typeof props[`${prop}Horizontal`]](
+			props,
+			prop,
+			props[`${prop}Horizontal`]
+		)};`
 });
 
 export default {
-	propTypes,
-	defaultProps,
+	propTypes: { ...propTypes("padding"), ...propTypes("margin") },
+	defaultProps: { ...defaultProps("padding"), ...defaultProps("margin") },
 	rules: { ...rules("padding"), ...rules("margin") }
 };
